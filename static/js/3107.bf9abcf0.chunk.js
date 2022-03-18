4c2bdcda81f9a2b7d04958d517cee6bbceecc077
Julien OLLIVIER <julien.ollivier@suricats-consulting.com>
2022-03-18 09:46:42 +0100

    Updates

---
+++ static/js/3107.bf9abcf0.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 3107.bf9abcf0.chunk.js.LICENSE.txt */
+"use strict";(self.webpackChunkbe_water=self.webpackChunkbe_water||[]).push([[3107],{3107:function(e,n,r){r.r(n),r.d(n,{g:function(){return t}});var t=r(7923).i}}]);
+//# sourceMappingURL=3107.bf9abcf0.chunk.js.map
